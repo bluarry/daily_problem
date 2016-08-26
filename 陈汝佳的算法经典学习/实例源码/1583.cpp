0cee77d963d3dc6560bd9688b0e2e06d92ae56aa
bluarry <1002587829@qq.com>
2016-08-27 00:17:06 +0800

    简单题

---
+++ 陈汝佳的算法经典学习/实例源码/1583.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define maxn 100005
+int ans[maxn];
+int main()
+{
+	int T,n;
+	memset(ans,0,sizeof(ans));
+
+	for(int m=1;m<maxn;m++)
+	{
+		int x=m,y=m;
+		while(x>0) 
+		{
+			y+=x%10;
+			x/=10;
+		}
+		if(ans[y]==0 || ans[y] > m) ans[y]=m;
+	}
+	scanf("%d",&T);
+	while(T--)
+	{
+		scanf("%d",&n);
+		printf("%d\n",ans[n]);
+	}
+
+	return 0;
+}
