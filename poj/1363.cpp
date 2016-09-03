892650914a4d00cff72ec344ef53bae626ccd74b
bluarry <1002587829@qq.com>
2016-09-03 10:16:44 +0800

    完成昨天落下的

--- poj/1363.cpp
+++ poj/1363.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <stack>
 #include <iostream>
+//#define LOCAL
 using namespace std;
 const int maxn=1000+10;
 
@@ -8,20 +9,36 @@ int n,target[maxn];
 
 int main()
 {
-	//freopen("D:\\1.in","r",stdin);
-	while(cin >> n){
-		stack<int > s;
-		int A=1,B=1; 
-		for(int i=1;i<=n;i++)
+	#ifdef LOCAL
+	freopen("D:\\1.in","r",stdin);
+	#endif
+	while(cin >> n && n ){
+		while(cin>> target[1] && target[1])
+		{
+		for(int i=2;i<=n;i++)
 			cin >> target[i];
+			stack<int > s;
+		int A=1,B=1; 
 		int ok=1;
-		while(B<=n){
-			if(A==target[B]) {A++;B++;}
-			else if(!s.empty() && s.top() == target[B]) {s.pop();B++;}
+		while(B<=n)
+		{
+			if(A==target[B]) 
+			{
+				A++;
+				B++;
+			}
+			else if(!s.empty() && s.top() == target[B])
+			 {
+			 	s.pop();
+				 B++;
+			}
 			else if(A<=n) s.push(A++);
 			else {ok=0;break;}
-	}
-	cout <<  (ok?"yes":"no") << endl;
+		}
+		
+		cout <<  (ok?"Yes":"No") << endl;
+		}
+		cout << endl;
 	}
 return 0;
 }
