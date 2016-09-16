893091dfcad578e61ef1a8843339421efd566f24
bluarry <1002587829@qq.com>
2016-09-16 22:53:07 +0800

    更新矩阵图

--- 严蔚敏数据结构/一些模板/图的矩阵表示及操作集.cpp
+++ 严蔚敏数据结构/一些模板/图的矩阵表示及操作集.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <string>
 #include <cstdio>
 using namespace std;
 
 #define INFINITY INT_MAX;//定义无穷大
 typedef int InfoType;
+struct VertexType{
+	string name;
+};
 typedef int VRType;//定义顶点之间的关系为整形
 #define MAX_VERTEX_NUM 30 //定义最大的顶点数
 enum GraphKind{DG,DN,UDG,UDN};//定义图的类型，，分别为有向图，有向网，无向图，无向网
@@ -11,7 +15,87 @@ typedef struct { // 定义每一条边的信息
 	VRType adj; 	//顶点的关系 1/权重
 	InfoType* info;  //这条边的相关信息
 }ArcCell,AdjMatrix[MAX_VERTEX_NUM][MAX_VERTEX_NUM];
+typedef struct {
+	VertexType vexs[MAX_VERTEX_NUM];
+	AdjMatrix arcs;
+	int vexnum,arcnum;
+	GraphKind kind;
+}MGraph;
+//定义数组表示
+
+
+
+void InputArc(InfoType* &arc){
+	string s;
+	cout << "请输入该弧的所有信息 : ";
+	cin >>s;	
+	arc = new char [s.length()+1];
 	
+	arc=s;
+}
+
+
+
+
+
+int locateVex(MGraph G,VertexType u){
+	for(int i=0;i<G.vexnum;i++)
+		if(u.name == G.vexs[i])
+			return i;
+
+return -1;
+}
+void CreateDG(MGraph& G){
+	cout << "请输入顶点的数，弧数，以及是否含义相关信息(是:1 否:0) : ";
+	int IncInf;
+	cin >> G.vexnum >> G.arcnum >> IncInf ;
+	cout << "请输入" << G.arcnum<< "个顶点的值(名称<"<<30 << "个字符" << endl;
+	 for(int i=0;i<G.vexnum;i++)	//输入每个点的信息
+	 	cin >> G.vexs[i];
+	 for(int i=0;i<G.vexnum;i++)	//初始化
+	 	for(int j=0;j<G.vexnum;j++)
+	 	{
+	 		G.arcs[i][j].adj=0;
+	 		G.arcs[i][j].info=NULL;
+	 	}
+	 cout << "请输入 "<< G.arcnum << " 条弧的弧头, 弧尾: " <<endl;
+	 VertexType v1,v2;
+	 for(int i=0;i<G.arcnum;i++){
+	 	cin >> v1.name >> v2.name;
+
+	 	int i=locateVex(G,v1);
+	 	int j=locateVex(G,v2);
+
+	 	G.arcs[i][j].adj=1;
+
+	 	if(IncInf)
+	 		InputArc(G.arcs[i][j].info);
+	 }
+	 G.kind=DG;
+}
+
+
+
+
+Status CreateGraph(MGraph& G){
+	cout << "请输入图G的类型(有向图 :0 无向图 :1 有向网 :2 无向网 :3) :" ;
+	cin >> G.kind;
+	switch(G.kind){
+		case DG:
+				CreateDG(G);
+				break;
+		case DN :
+				CreateDN(G);
+				break;
+		case UDG :
+				CreateUDG(G);
+				break;
+		case UDN :
+				CreateUDN(G);
+				break;		
+	}
+}
+
 
 
 
